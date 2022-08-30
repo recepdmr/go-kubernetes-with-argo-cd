658a96ddecceb617b6a26204a26d528489d41e8b
Recep Demir <recepdemir@Receps-MacBook-Pro.local>
2022-08-30 19:56:03 +0300

    Changed response text

--- main.go
+++ main.go
@@ -9,7 +9,7 @@ import (
 
 func main() {
 	http.HandleFunc("/", func(w http.ResponseWriter, r *http.Request) {
-		w.Write([]byte("hello"))
+		w.Write([]byte("hi"))
 	})
 
 	port := fmt.Sprintf(":%s", os.Getenv("PORT"))
